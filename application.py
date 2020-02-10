e68ee59f3a51280bd052a6ae414c5cabf9e4ed67
tskluzac <skluzacek@uchicago.edu>
2020-02-10 10:25:51 -0600

    Adding eb configs so this works.

---
+++ application.py
@@ -0,0 +1,58 @@
+
+from flask import Flask, request
+from flask_api import status
+
+from crawlers.globus_base import GlobusCrawler
+from uuid import uuid4
+
+import threading
+
+application = Flask(__name__)
+
+
+def crawl_launch(crawler, tc):
+    crawler.crawl(tc)
+
+
+@application.route('/')
+def hello():
+    st = status.HTTP_200_OK
+    return f"Welcome to the Xtract crawler! \n Status: {str(st)}", st
+
+
+@application.route('/crawl', methods=['POST'])
+def crawl_repo():
+
+    r = request.json
+
+    endpoint_id = r['eid']
+    starting_dir = r['dir_path']
+    grouper = r['grouper']
+    transfer_token = r['Transfer']
+    auth_token = r['Authorization']
+
+    print(f"Received Transfer Token: {transfer_token}")
+
+    crawl_id = uuid4()
+    crawler = GlobusCrawler(endpoint_id, starting_dir, crawl_id, transfer_token, auth_token, grouper)
+    tc = crawler.get_transfer()
+    crawl_thread = threading.Thread(target=crawl_launch, args=(crawler, tc))
+    crawl_thread.start()
+
+    return {"crawl_id": str(crawl_id)}, status.HTTP_200_OK
+
+
+# @application.route('/get_crawl_status', methods=['GET'])
+# def get_cr_status():
+#
+#     r = request.json
+#
+#     crawl_id = r["crawl_id"]
+#     resp = get_crawl_status(crawl_id)
+#     print(resp)
+#
+#     return resp
+
+
+if __name__ == '__main__':
+    application.run(debug=True, threaded=True)
