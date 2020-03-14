8977df72a06794769745401b504716b509bab4bd
tskluzac <skluzacek@uchicago.edu>
2020-03-13 22:12:31 -0500

    Crawler working with correct status.

--- crawlers/globus_base.py
+++ crawlers/globus_base.py
@@ -113,7 +113,7 @@ class GlobusCrawler(Crawler):
         time0 = time.time()
         auth_detail = conf_app_client.oauth2_token_introspect(token)
         time1 = time.time()
-        print(f"INTROSPECT TIME: {time1-time0}")
+        logging.info(f"INTROSPECT TIME: {time1-time0}")
 
         uid = auth_detail['username']
 
@@ -144,7 +144,6 @@ class GlobusCrawler(Crawler):
     def launch_crawl_worker(self, transfer, worker_id):
         logging.basicConfig(format=f"%(asctime)s - %(message)s', filename='crawler_{worker_id}.log", level=logging.INFO)
 
-
         self.worker_status_dict[worker_id] = "STARTING"
 
         while True:
@@ -154,14 +153,14 @@ class GlobusCrawler(Crawler):
             if self.to_crawl.empty():
                 # This worker sees an empty queue, AND IF NOT ALREADY "IDLE", should become "IDLE"
                 if self.worker_status_dict[worker_id] is not "IDLE":
-                    print(f"Worker ID: {worker_id} demoted to IDLE.")
+                    logging.info(f"Worker ID: {worker_id} demoted to IDLE.")
                     self.worker_status_dict[worker_id] = "IDLE"
                     self.idle_worker_count += 1
 
                 # If to_crawl is empty, we want to check and see if other crawl_workers idle AND not in 'starting state'
                 # If all of the workers are idle AND state != 'STARTING'.
                 if self.idle_worker_count >= self.max_crawl_threads:
-                    print(f"Worker ID: {worker_id} is terminating.")
+                    logging.info(f"Worker ID: {worker_id} is terminating.")
                     return "CRAWL--COMPLETE"  # TODO: Behavior for collapsing a thread w/ no real return val?
                 continue
 
@@ -172,8 +171,8 @@ class GlobusCrawler(Crawler):
                     cur_dir = self.to_crawl.get()
                     restart_loop = False
                 except Exception as e:
-                    print("Caught the following race condition exception... ignoring...")
-                    print(e)
+                    logging.error("Caught the following race condition exception... ignoring...")
+                    logging.error(e)
 
                     # Go back to beginning and check queue again.
                     continue
@@ -181,7 +180,7 @@ class GlobusCrawler(Crawler):
             # In the case where we successfully extracted from queue AND worker not "ACTIVE", make it active.
             if self.worker_status_dict[worker_id] is not "ACTIVE":
                 self.worker_status_dict[worker_id] = "ACTIVE"
-                print(f"Worker ID: {worker_id} promoted to ACTIVE.")
+                logging.info(f"Worker ID: {worker_id} promoted to ACTIVE.")
 
             try:
                 while True:
@@ -189,7 +188,6 @@ class GlobusCrawler(Crawler):
                         dir_contents = transfer.operation_ls(self.eid, path=cur_dir)
                         break
 
-                    # TODO: Be less broad here.
                     except GlobusTimeoutError as e:
                         logging.error("Globus Timeout Error -- retrying")
                         logging.info(e)
@@ -197,7 +195,7 @@ class GlobusCrawler(Crawler):
 
                     except Exception as e:
 
-                        print(str(e))
+                        logging.error(str(e))
                         if '502' in str(e)[0:4]:
                             logging.error("Directory too large...")
                             restart_loop = True
@@ -214,7 +212,7 @@ class GlobusCrawler(Crawler):
                 f_names = []
                 for entry in dir_contents:
 
-                    # print(f"[DEBUG] Entry: {entry}")
+                    logging.debug(f"[DEBUG] Entry: {entry}")
 
                     full_path = cur_dir + "/" + entry['name']
                     if entry['type'] == 'file':
@@ -222,15 +220,9 @@ class GlobusCrawler(Crawler):
                         f_names.append(full_path)
                         extension = self.get_extension(entry["name"])
 
-                        # print(f"Metadata for full path: {entry}")
+                        logging.debug(f"Metadata for full path: {entry}")
                         all_file_mdata[full_path] = {"physical": {'size': entry['size'],
                                                               "extension": extension, "path_type": "globus"}}
-
-                        if full_path == "/MDF/mdf_connect/prod/data/_test_mayer_situ_observation_g20o995_v1.1/Ag3Al.cP20/OUTCAR":
-                            print("************************************************* (here)")
-                            # exit()
-
-                        # print(f"[DEBUG] Metadata blob: {all_file_mdata}")
                     elif entry['type'] == 'dir':
                         full_path = cur_dir + "/" + entry['name']
                         self.to_crawl.put(full_path)
@@ -238,54 +230,36 @@ class GlobusCrawler(Crawler):
                     else:
                         raise Exception("Fucking hell")
 
-                print(f"Finished parsing files. Metadata: {all_file_mdata}")
+                logging.debug(f"Finished parsing files. Metadata: {all_file_mdata}")
 
                 # Step 2. We want to process each potential group of files.
                 gr_dict = self.grouper.group(f_names)
-                print(f"gr_dict: {gr_dict}")
+                logging.debug(f"gr_dict: {gr_dict}")
 
                 # Step 3. For all parsers...
                 for parser in gr_dict:
-                    print(f"Parser: {parser}")
+                    logging.debug(f"Parser: {parser}")
 
                     # Cast as generator for debugging.
                     gr_list = list(gr_dict[parser])
-                    # TODO print(f"gr_list: {gr_list}")
 
                     # Step 4. For each group within a parser
                     for gr in gr_list:
 
-                        print("IN GROUP-BY-PARSER LOOP...")
-                        print(f"Group Tuple: {gr}")
-
-                        # time.sleep(1)
+                        logging.debug("IN GROUP-BY-PARSER LOOP...")
+                        logging.debug(f"Group Tuple: {gr}")
 
                         gr_id = str(self.gen_group_id())
                         group_info = {"group_id": gr_id, "parser": parser, "files": [], "mdata": []}
 
                         file_list = list(gr)
-                        # TODO
-                        # print(f"File list: {file_list}")
 
                         group_info["files"] = file_list
 
-                        # if len(file_list) > 0:
-                        #     raise Exception("derpderpderp")
-
                         for f in file_list:
-                            # try:
-                                # print(f"Group Info: {group_info}")
-
                             group_info["mdata"].append({"file": f, "blob": all_file_mdata[f]})
-                            # except Exception as e:
-                            #     #print("ERROR... ")
-                            #     print(f"Metadata Blob: {all_file_mdata}")
-                            #     print("Led to exception...")
-                            #     time.sleep(1)
-                            #     raise(e)
 
                         logging.info(group_info)
-
                         cur = self.conn.cursor()
 
                         try:
@@ -298,32 +272,22 @@ class GlobusCrawler(Crawler):
                             logging.error("Continuing!")
 
                         else:
-                            # TODO: This try/except exists only because of occasinoal pg char issue -- should fix.
-                            # try:
+                            # TODO: Occasional pg char issue -- should fix.
                             query = f"INSERT INTO group_metadata_2 (group_id, metadata, files, parsers, owner) " \
                                 f"VALUES ('{gr_id}', {psycopg2.Binary(pkl.dumps(group_info))}, '{files}', '{parsers}', '{self.token_owner}')"
 
-                            print(group_info)
                             logging.info(f"Group Metadata query: {query}")
                             self.group_count += 1
                             cur.execute(query)
                             self.conn.commit()
-
                             self.add_group_to_db(str(group_info["group_id"]), len(group_info['files']))
-                            # except:
-                            #     logging.error("Failure pushing to postgres...")
-                            #     pass
 
-                        # all_file_mdata = {}
-
-            # TODO: Is this unnecessary now?
             except TransferAPIError as e:
                 logging.error("Problem directory {}".format(cur_dir))
                 logging.error("Transfer client received the following error:")
                 logging.error(e)
                 self.failed_dirs["failed"].append(cur_dir)
                 continue
-
 
     def crawl(self, transfer):
         dir_name = "./xtract_metadata"
@@ -351,7 +315,6 @@ class GlobusCrawler(Crawler):
         for t in list_threads:
             t.join()
 
-
         logging.info(f"\n***FINAL groups processed for crawl_id {self.crawl_id}: {self.group_count}***")
         logging.info(f"\n*** CRAWL COMPLETE  (ID: {self.crawl_id})***")
 
@@ -362,5 +325,3 @@ class GlobusCrawler(Crawler):
 
         with open('failed_groups.json', 'w') as gp:
             json.dump(self.failed_groups, gp)
-
-print("FORCE EB TO UPDATE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
