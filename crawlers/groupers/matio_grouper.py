d4af3281129f1c5c50fbbf4b012a33993a2194ad
tskluzac <skluzacek@uchicago.edu>
2020-04-13 12:14:31 -0500

    Cancel groups larger than 1000 files (for now).

--- crawlers/groupers/matio_grouper.py
+++ crawlers/groupers/matio_grouper.py
@@ -12,13 +12,16 @@ class MatIOGrouper:
     def __init__(self):
         self.name = "matio"  # TODO: Add to parent class.
         self.soft_max_files = 50
-        self.hard_max_files = 10000
+        self.hard_max_files = 1000
 
     def make_file_graph(self, group_dict):
+
+        # for group in group_dict:
 
         num_g_1 = 0
         print("Assembling graph")
         gr = nx.Graph()
+        too_long_comps = []
         groups = []
         for parser in group_dict:
             print(parser)
@@ -27,6 +30,10 @@ class MatIOGrouper:
                 if len(item) > 1:
                     # print(item)
                     num_g_1 += 1
+                    if len(item) >= 1000:
+                        print("TOO BIG")
+                        print(len(item))
+                        exit()
                     print(f"Number of groups with more than 1 element: {num_g_1}")
                     print(f"Len of big group: {len(item)}")
                 groups.append(item)
@@ -34,6 +41,7 @@ class MatIOGrouper:
         print("Generating nodes and edges... ")
         print(f"Number of groups: {len(groups)}")
         for group in groups:
+
             # Add nodes
             for filename in group:
                 gr.add_node(filename)
@@ -45,8 +53,23 @@ class MatIOGrouper:
 
         print("Generating number of connected components...")
         conn_comps = sorted(nx.connected_components(gr), key=len, reverse=True)
-        print(len(conn_comps))
+        print(f"Type of conn_comps: {type(conn_comps)}")
 
+        ### DEBUG
+
+        # for item in conn_comps:
+            # print(type(item))
+
+        ###
+
+        # for item in too_long_comps:
+        #     conn_comps.append(set(item))
+        # conn_comps.append(set(too_long_comps))
+        print(f"Number of connected components: {len(conn_comps)}")
+
+        print("Now counting nodes and edges...")
+
+        # exit()
         print(f"Number of nodes: {gr.number_of_nodes()}")
         print(f"Number of edges: {gr.number_of_edges()}")
 
@@ -65,8 +88,9 @@ class MatIOGrouper:
             family_uuid = str(uuid4())
 
             for filename in comp:
+                # print(f"Filename: {filename}")
                 gr_ids = file_groups_map[filename]
-                print(f"Group IDs: {gr_ids}")
+                # print(f"Group IDs: {gr_ids}")
 
                 families[family_uuid] = {"groups": {}}
 
@@ -99,9 +123,17 @@ class MatIOGrouper:
 
             gr_list = list(group)
 
-            group_coll[parser] = gr_list
+            # group_coll[parser] = gr_list
+            group_coll[parser] = []
 
             for gr in gr_list:
+
+                # TODO: Odd assumption to be making. Should allow groups to be arbitrarily large.
+                if len(gr) > self.hard_max_files:
+                    print("The group was too dang big. ")
+                    print(len(gr))
+                    continue
+
                 # Assign a group_id for each group.
                 group_id = str(uuid4())
                 for filename in gr:
@@ -112,6 +144,11 @@ class MatIOGrouper:
 
                     group_files_map[group_id] = {"files": [], "parser": parser}
                     group_files_map[group_id]["files"].append(filename)
+                group_coll[parser].append(gr)
+
+                if len(gr) > 1000:
+
+                    print("HOW THE FUCK DID WE GET HERE????")
 
         # Get the connected components of the graph.
         conn_comps = self.make_file_graph(group_coll)
