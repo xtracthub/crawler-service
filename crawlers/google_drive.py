b65c6b000b1e4eca3e6cdf9e2f178260e3a0d3f6
tskluzac <skluzacek@uchicago.edu>
2020-06-08 12:53:15 -0500

    Pushing crawl results to SQS

--- crawlers/google_drive.py
+++ crawlers/google_drive.py
@@ -1,8 +1,14 @@
 
 import datetime
+import threading
+import os
+import boto3
 from .base import Crawler
 from crawlers.utils.gdrive_utils import get_next_page, generate_drive_connection
 from crawlers.groupers import simple_ext_grouper
+from queue import Queue
+import time
+import json
 
 
 class GoogleDriveCrawler(Crawler):
@@ -17,11 +23,79 @@ class GoogleDriveCrawler(Crawler):
         self.count_files_crawled = 0
         self.count_bytes_crawled = 0
 
+        self.active_commits = 0
+
+        self.commit_threads = 5
+        self.families_to_enqueue = Queue()
+
         print("Generating connection to Google Drive API...")
         self.drive_conn = generate_drive_connection(self.creds)
         print("Connection to Drive API successful!")
 
         self.crawl_status = "STARTING"
+
+        # TODO: this really needs to be part of its own data class bc copied and pasted from globus_base
+        self.client = boto3.client('sqs',
+                                   aws_access_key_id=os.environ["aws_access"],
+                                   aws_secret_access_key=os.environ["aws_secret"], region_name='us-east-1')
+        print(f"Creating queue for crawl_id: {self.crawl_id}")
+        queue = self.client.create_queue(QueueName=f"crawl_{str(self.crawl_id)}")
+
+        if queue["ResponseMetadata"]["HTTPStatusCode"] == 200:
+            self.queue_url = queue["QueueUrl"]
+        else:
+            raise ConnectionError("Received non-200 status from SQS!")
+
+        self.sqs_push_threads = {}
+        self.thr_ls = []
+        for i in range(0, self.commit_threads):
+            thr = threading.Thread(target=self.enqueue_loop, args=(i,))
+            self.thr_ls.append(thr)
+            thr.start()
+            self.sqs_push_threads[i] = True
+        print(f"Successfully started {len(self.sqs_push_threads)} SQS push threads!")
+
+    def enqueue_loop(self, thr_id):
+
+        while True:
+            insertables = []
+
+            # If empty, then we want to return.
+            if self.families_to_enqueue.empty():
+                # If ingest queue empty, we can demote to "idle"
+                if self.crawl_status == "COMMITTING":
+                    self.sqs_push_threads[thr_id] = "IDLE"
+                    print(f"Thread {thr_id} is committing and idle!")
+                    time.sleep(0.25)
+
+                    # NOW if all threads idle, then return!
+                    if all(value == "IDLE" for value in self.sqs_push_threads.values()):
+                        self.commit_completed = True
+                        self.crawl_status = "SUCCESS"
+                        print(f"Thread {thr_id} is terminating!")
+                        return 0
+                time.sleep(1)
+                continue
+
+            self.sqs_push_threads[thr_id] = "ACTIVE"
+
+            # Remove up to n elements from queue, where n is current_batch.
+            current_batch = 1
+            while not self.families_to_enqueue.empty() and current_batch < 10:
+                insertables.append(self.families_to_enqueue.get())
+                self.active_commits -= 1
+                current_batch += 1
+
+            print(f"Insertables: {insertables}")
+
+            print("[COMMIT] Preparing batch commit -- executing!")
+
+            try:
+                response = self.client.send_message_batch(QueueUrl=self.queue_url,
+                                                          Entries=insertables)
+                print(f"SQS response: {response}")
+            except Exception as e:  # TODO: too vague
+                print(f"WAS UNABLE TO PROPERLY CONNECT to SQS QUEUE: {e}")
 
     def crawl(self, tc=None):  # TODO: Find clean way to remove transfer_client requirement from .crawl() function.
         # TODO: Properly generate the queues for these.
@@ -30,6 +104,7 @@ class GoogleDriveCrawler(Crawler):
         starting = True
 
         grouper = simple_ext_grouper.SimpleExtensionGrouper()
+        file_count = 0
 
         while True:
 
@@ -47,6 +122,10 @@ class GoogleDriveCrawler(Crawler):
             clean_mdata = self.gdrive_mdata_cleaner(items)
             all_files.extend(clean_mdata)
 
+            for item in clean_mdata:
+                self.families_to_enqueue.put({"Id": str(file_count), "MessageBody": json.dumps(item)})
+                file_count += 1
+
             self.count_files_crawled += len(items)
 
             if not next_page_token and not starting:
@@ -54,8 +133,9 @@ class GoogleDriveCrawler(Crawler):
                 print(f"Total files processed: {len(all_files)}")
                 # TODO: this should return, but should probably enqueue (same as Globus HTTPS crawler)
                 # return {"file_mdata": all_files, "crawl_id": self.crawl_id}
-                self.crawl_status = "SUCCEEDED"
+                self.crawl_status = "COMMITTING"
                 break
+
         print("Running grouper...")
         grouped_mdata = grouper.gen_groups(all_files)
 
@@ -79,6 +159,7 @@ class GoogleDriveCrawler(Crawler):
                 print(t)
                 raise ValueError
         print(f"Text: {text_tally}\nTabular: {tab_tally}\nImages: {im_tally}\nNone: {none_tally}")
+
 
     def gdrive_mdata_cleaner(self, results_ls):
 
