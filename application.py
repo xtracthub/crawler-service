41f83f43953b0779283271a887016a52ca627e71
tskluzac <skluzacek@uchicago.edu>
2021-03-01 15:03:26 -0600

    I think we uncovered the fix in Forge.

--- application.py
+++ application.py
@@ -37,6 +37,37 @@ box_creds = dict()
 log = logging.getLogger('werkzeug')
 
 
+from globus_sdk import ConfidentialAppAuthClient
+import time
+import os
+
+
+def get_uid_from_token(auth_token):
+    # Step 1: Get Auth Client with Secrets.
+    client_id = os.getenv("GLOBUS_FUNCX_CLIENT")
+    secret = os.getenv("GLOBUS_FUNCX_SECRET")
+
+    # Step 2: Transform token and introspect it.
+    t0 = time.time()
+
+    conf_app_client = ConfidentialAppAuthClient(client_id, secret)
+    token = str.replace(str(auth_token), 'Bearer ', '')
+
+    auth_detail = conf_app_client.oauth2_token_introspect(token)
+    t1 = time.time()
+
+    try:
+        print(auth_detail)
+        uid = auth_detail['username']
+    except KeyError as e:
+        raise ValueError("Unable to identify Globus user. Returning (to reject!)")
+
+    print(f"Authenticated user {uid} in {t1-t0} seconds")
+
+    # print(uid)
+    return uid
+
+
 def store_tokens(access_token, refresh_token):
 
     cwd = os.getcwd()
@@ -88,6 +119,9 @@ def crawl_repo():
     tokens = r['tokens']  # TODO: no idea why this is arriving as a list.
 
     print(tokens)
+
+    # TODO: SOMETHING HERE.
+    print(get_uid_from_token(tokens['Authorization']))
 
     # if repo_type == "GLOBUS":
     # endpoint_id = r['eid']
